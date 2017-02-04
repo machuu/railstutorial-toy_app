942e0b13c061e4acd0aa21b81282a4a317dc5799
Matt Hoover <matt@hoover.ml>
2017-02-04 05:12:19 +0000

    Update routes from 'hello...' to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def hello
-    render html: "hello, world!"
+  def index
+    @users = User.all
   end
 end
